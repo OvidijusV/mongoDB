d5487258d1f5e349685f225a7fb70e473ce48f2b
OvidijusV <ovidijusv14@gmail.com>
2022-10-19 20:06:28 +0300

    embedded and aggregation outputs

--- mongo.py
+++ mongo.py
@@ -15,10 +15,34 @@ def dataInsert():
     ownersCol.insert_many([owners.owner1, owners.owner2, owners.owner3])
     vehiclesCol.insert_many([vehicles.vehicle1, vehicles.vehicle2, vehicles.vehicle3, vehicles.vehicle4, vehicles.vehicle5])
     
+def getEmbedded():
+    for i in ownersCol.find({}, {"Name", "Surname", "Address", "Vehicles"}):
+        print(f"{i['Name']} {i['Surname']}:\n Address: {i['Address']}\n Vehicles: {i['Vehicles']}")
+
+def aggregation():
+    agrInput = [
+        {
+            "$lookup":
+                {
+                    "from": "Vehicles",
+                    "localField": "Vehicles.VehicleID",
+                    "foreignField": "_id",
+                    "as": "Vehicles"
+                }
+
+    }
+    ]
+    agrOutput = ownersCol.aggregate(agrInput)
+    for owner in agrOutput:
+        print("Owner: {own}\nHas vehicles:".format(own=owner['Name']+ " " +owner['Surname']))
+        for car in owner['Vehicles']:
+            print("{make} {model} {engineDis}cc {kw}".format(make=car['Make'], model=car['Model'], engineDis=car['Engine']['DisplacementCC'], kw=car['Engine']['Fuel']))
 
 # Main
 def main():
     dataInsert()
+    getEmbedded()
+    aggregation()
 
 
 if __name__ == "__main__":
--- vehicles.py
+++ vehicles.py
@@ -2,63 +2,68 @@ vehicle1 = {
     "_id":1,
     "Make": "Audi",
     "Model": "80",
-    "Enginge": {
+    "Engine": {
         "DisplacementCC": 1998,
         "PowerKW": 125,
         "Fuel": "Petrol" 
     },
     "TechnicalInspection":True,
-    "ProductionDate":"1997-10-24"
+    "ProductionDate":"1997-10-24",
+    "PaidEU": 150
 }
 
 vehicle2 = {
     "_id":2,
     "Make": "BMW",
     "Model": "E46",
-    "Enginge": {
+    "Engine": {
         "DisplacementCC": 2998,
         "PowerKW": 170,
         "Fuel": "Petrol" 
     },
     "TechnicalInspection":True,
-    "ProductionDate":"2003-04-15"
+    "ProductionDate":"2003-04-15",
+    "PaidEU": 2500
 }
 
 vehicle3 = {
     "_id":3,
     "Make": "VW",
-    "Model": "Golf",
-    "Enginge": {
+    "Model": "Passat",
+    "Engine": {
         "DisplacementCC": 2000,
         "PowerKW": 110,
         "Fuel": "Diesel" 
     },
     "TechnicalInspection":False,
-    "ProductionDate":"2005-11-30"
+    "ProductionDate":"2005-11-30",
+    "PaidEU": 1500
 }
 
 vehicle4 = {
     "_id":4,
     "Make": "VW",
     "Model": "Sharan",
-    "Enginge": {
+    "Engine": {
         "DisplacementCC": 1900,
         "PowerKW": 86,
         "Fuel": "Diesel" 
     },
     "TechnicalInspection":True,
-    "ProductionDate":"2001-02-07"
+    "ProductionDate":"2001-02-07",
+    "PaidEU": 999
 }
 
 vehicle5 = {
     "_id":5,
     "Make": "BMW",
     "Model": "E36",
-    "Enginge": {
+    "Engine": {
         "DisplacementCC": 2800,
         "PowerKW": 142,
         "Fuel": "Petrol" 
     },
     "TechnicalInspection":True,
-    "ProductionDate":"1996-09-27"
+    "ProductionDate":"1996-09-27",
+    "PaidEU": 4300
 }
